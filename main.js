abe9b0b233b44483e930176129bded1e1f54c523
Eduardo de la Cruz Palacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-01-17 22:11:38 +0100

    services_done

--- main.js
+++ main.js
@@ -12,3 +12,33 @@ function typing()
         setTimeout(typing, speed);
     }
 };
+
+/* LLUVIA ICONOS */
+
+// do a function (what happens when called)
+function createIcon()
+{
+    // constant box = to create img element
+    const box = document.createElement("img");
+    // make img have src="shield.svg" attribute
+    box.setAttribute("src","star.svg");
+
+    box.style.position = "absolute";
+
+    // (for img element) left= ; attribute has 0-98vh as value
+    box.style.left = Math.random() * 98 + "%";
+
+    // (for img element) animationDuration attribute has 3-4s as value
+    box.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+    // make img be in #lluvia element
+    document.getElementById("lluvia").appendChild(box);
+    // now, in the body we have an img
+
+    // time for each img to disapper
+    setTimeout(() => {
+        box.remove();
+    }, 2000);
+}
+// set time for our function to be executed
+setInterval(createIcon, 70);
