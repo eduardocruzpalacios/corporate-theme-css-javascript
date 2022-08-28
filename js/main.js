1f8bdf0b85ca0f39561da37cef8952fd438ec2df
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-08-28 10:37:54 +0200

    Format JS

--- js/main.js
+++ js/main.js
@@ -5,35 +5,35 @@ const str = 'Let us show you how we unleash knowledge unlimited power';
 const speed = 100;
 
 function Typing() {
-    if (i < str.length) {
-        document.getElementById('hero').innerHTML += str.charAt(i);
-        i++;
-        setTimeout(Typing, speed);
-    }
+  if (i < str.length) {
+    document.getElementById('hero').innerHTML += str.charAt(i);
+    i++;
+    setTimeout(Typing, speed);
+  }
 }
 
 /* ICON RAIN */
 
 function CreateIcon() {
-    const box = document.createElement('img');
-    box.setAttribute('src', '../icons/star.svg');
-    box.style.position = 'absolute';
-    box.style.left = Math.random() * 98 + '%';
-    box.style.animationDuration = Math.random() * 2 + 3 + 's';
-    document.getElementById('rainContainer').appendChild(box);
+  const box = document.createElement('img');
+  box.setAttribute('src', '../icons/star.svg');
+  box.style.position = 'absolute';
+  box.style.left = Math.random() * 98 + '%';
+  box.style.animationDuration = Math.random() * 2 + 3 + 's';
+  document.getElementById('rainContainer').appendChild(box);
 
-    setTimeout(() => {
-        box.remove();
-    }, 2000);
+  setTimeout(() => {
+    box.remove();
+  }, 2000);
 }
 setInterval(CreateIcon, 70);
 
 // NAV MENU
 
 function OpenNav() {
-    document.getElementById('myNav').style.width = '100%';
+  document.getElementById('myNav').style.width = '100%';
 }
 
 function CloseNav() {
-    document.getElementById('myNav').style.width = '0%';
+  document.getElementById('myNav').style.width = '0%';
 }
